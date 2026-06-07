289c3fc7c0784a03ac9ebef9e0cc95ebdbe6d0ac
Gonza <gonzalogacc@gmail.com>
2026-06-07 12:32:45 +0000

    Fix the requester a bit. Make a loop for the announcer in the main loop

--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+import threading
+from time import sleep
 import RNS
 
 from config import settings
-from identity import announce_destination, create_identity
+from identity import create_identity
 
 from router import Router
 from utils import client_connected
@@ -13,7 +15,7 @@ from utils import client_connected
 
 class Server:
     
-    def __init__(self, config_path: Path | None = None):
+    def __init__(self, config_path: Path):
 
         # A reference to the latest client link that connected
         self.latest_client_link = None
@@ -21,23 +23,26 @@ class Server:
 
         self.reticulum = RNS.Reticulum(self.config_path)
 
+        identity = create_identity(Path(settings.IDENTITY_PATH))
         self.server_destination = RNS.Destination(
-            create_identity(settings.IDENTITY_PATH),
+            identity,
             RNS.Destination.IN,
             RNS.Destination.SINGLE,
             settings.APP_NAME,
             "requestexample"
         ) 
 
-        self.set_server()
-
-    def __call__(self):
-        self.server_loop(self.server_destination)
-    
-    def set_server(self):
-        # We must first initialise Reticulum
+        # Set callbacks.
+        # TODO (Gonza): move this to a function later for easier configuration
         self.server_destination.set_link_established_callback(client_connected)
 
+    def __call__(self):
+        RNS.log(f"Running server.")
+        self.server_loop(self.server_destination)
+    
+    def announce_destination(self, destination):
+        destination.announce()
+        RNS.log(f"Sent announce from {RNS.prettyhexrep(destination.hash)}")
 
     def add_router(self, router: Router):
         for router_inst in router.ROUTES_REGISTRY:
@@ -48,15 +53,9 @@ class Server:
             )
 
     def server_loop(self, destination):
-        # Let the user know that everything is ready
-        RNS.log(
-            "Request example "+
-            RNS.prettyhexrep(destination.hash)+
-            " running, waiting for a connection."
-        )
+        RNS.log(f"Request example {RNS.prettyhexrep(destination.hash)} running, waiting for a connection.")
 
-        RNS.log("Hit enter to manually send an announce (Ctrl-C to quit)")
-
-        # Send the destination 
-        with ThreadPoolExecutor(max_workers=1) as executor:
-            executor.submit(announce_destination, destination)
+        # This look keeps the server alive and annouces the server every N seconds
+        while True:
+            self.announce_destination(destination)
+            sleep(settings.ANNOUNCE_INTERVAL_SECONDS)
